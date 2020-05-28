75d2b3ebf97013007e384ed33999b652a20807c0
ritambasu61 <57460456+ritambasu61@users.noreply.github.com>
2020-05-28 14:43:09 +0530

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -38,6 +38,9 @@ elif (1.0<p1_V_greter_v1<5.0 or 95.0<p1_V_greter_v1<99.0)and (1.0<p2_V_greter_v2
     print('Random numbers are suspect')
 elif (5.0<p1_V_greter_v1<10.0 or 90.0<p1_V_greter_v1<95.0)and (5.0<p2_V_greter_v2<10.0 or 90.0<p2_V_greter_v2<95.0):
     print('Random numbers are almost suspect')   
-else:
+elif(10.0<p1_V_greter_v1<90.0)and(10.0<p2_V_greter_v2<90.0):
     print('Numbers are sufficiently random')
+else:
+    print('nothing can be said')
+    
     
